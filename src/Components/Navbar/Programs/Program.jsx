e2290299134b00ded0caa42d1c3ff0a2bd95cc13
sridharan377 <sridharan3770@gmail.com>
2024-08-08 18:16:14 +0530

    yes

--- src/Components/Navbar/Programs/Program.jsx
+++ src/Components/Navbar/Programs/Program.jsx
@@ -14,12 +14,12 @@ const Programs = () => {
     <div className='Programs'>
         
         <div className="TestSection">
-        <h2>Website Recon</h2>
-        <p>Find out which web technologies your target website is using.</p>
+        
+        
         <div className="input-group">
           <label htmlFor="protocol">Target</label>
           
-          <input type="text" placeholder="www.example.com" />
+          <input type="text" placeholder="www.amrita.com" />
         </div>
         <button className="submit-btn">Start scan</button>
       </div>
